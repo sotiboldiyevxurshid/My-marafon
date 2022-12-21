ed9e54dbd249705d073e4b1239079fc1b2854d3a
Xurshid <sotiboldiyevxurshid21@gmail.com>
2022-12-21 14:21:18 +0500

    salom

---
+++ index.js
@@ -0,0 +1,20 @@
+    
+    function substraction (num1,num2){
+        return num1 - num2
+    }
+    console.log(substraction(110, 21));
+    console.log( substraction(1000, 122));
+    console.log(substraction(10, 98));
+
+
+//     https://github.com/ali/marafon/01-011222.js
+// Sana: 17:00 20-12-22
+// Muallif: Sotiboldiyev xurshid (Frontend 04)
+// Masala: Ayirmani toping
+    
+    
+    
+    
+    
+    
+   
